437d6da18e70eb81182b17d86e22d4e550152e29
DESKTOP-JKUCR97\Artem <budzinski212@o2.pl>
2024-11-23 21:43:24 +0100

    feat(service): remove comments

--- CVGeneratorService/CVGeneratorService/Controllers/PdfController.cs
+++ CVGeneratorService/CVGeneratorService/Controllers/PdfController.cs
@@ -39,18 +39,18 @@ public class PdfController : ControllerBase
             AboutMe = new AboutMe 
             { 
                 ColumnPosition = ColumnPosition.Left,
-                FirstName = "John",  // Zmieniono na fikcyjne imię
-                LastName = "Doe",    // Zmieniono na fikcyjne nazwisko
-                Job = "Software Engineer", // Zmieniono na fikcyjny zawód
+                FirstName = "John",
+                LastName = "Doe",
+                Job = "Software Engineer",
                 AboutMeText =
                     "Passionate software engineer with a love for coding and learning new technologies. Always eager to take on new challenges and improve problem-solving skills."
             },
             Contact = new Contact
             {
-                Born = new DateTime(1990, 5, 15),  // Zmieniono datę urodzenia
-                Address = "123 Fake Street, Faketown, FT 12345", // Zmieniono adres
-                Email = "john.doe@example.com", // Zmieniono email
-                Phone = "555-1234-5678", // Zmieniono numer telefonu
+                Born = new DateTime(1990, 5, 15),
+                Address = "123 Fake Street, Faketown, FT 12345",
+                Email = "john.doe@example.com",
+                Phone = "555-1234-5678"
             },
             LanguageSection = new LanguageSection
             {
@@ -71,7 +71,7 @@ public class PdfController : ControllerBase
                     {
                         StartDate = new DateTime(2010, 9, 1), 
                         EndDate = new DateTime(2014, 6, 1),
-                        InstitutionType = "Faketown University", // Zmieniono na fikcyjną uczelnię
+                        InstitutionType = "Faketown University",
                         DegreeTitle = "Bachelor's Degree",
                         Faculty = "Faculty of Computer Science",
                         AdditionalInfo = "Graduated with honors"
@@ -80,7 +80,7 @@ public class PdfController : ControllerBase
                     {
                         StartDate = new DateTime(2015, 9, 1), 
                         EndDate = new DateTime(2017, 6, 1),
-                        InstitutionType = "Faketown University", // Zmieniono na fikcyjną uczelnię
+                        InstitutionType = "Faketown University",
                         DegreeTitle = "Master's Degree",
                         Faculty = "Faculty of Software Engineering",
                         AdditionalInfo = "Specialization in Machine Learning"
@@ -96,16 +96,16 @@ public class PdfController : ControllerBase
                     {
                         StartDate = new DateTime(2015, 6, 1),
                         EndDate = new DateTime(2018, 6, 1),
-                        CompanyName = "TechCorp", // Zmieniono na fikcyjną firmę
-                        Position = "Junior Developer", // Zmieniono na fikcyjny tytuł
+                        CompanyName = "TechCorp",
+                        Position = "Junior Developer",
                         ExperienceDescriptions = new List<string> {"Worked on web applications", "Collaborated with a cross-functional team", "Participated in agile sprint planning"}
                     },
                     new()
                     {
                         StartDate = new DateTime(2018, 7, 1),
                         EndDate = new DateTime(2023, 1, 1),
-                        CompanyName = "CodeWorks", // Zmieniono na fikcyjną firmę
-                        Position = "Senior Developer", // Zmieniono na fikcyjny tytuł
+                        CompanyName = "CodeWorks",
+                        Position = "Senior Developer",
                         ExperienceDescriptions = new List<string> {"Led a team of developers", "Developed REST APIs", "Improved software performance and scalability"}
                     }
                 }
