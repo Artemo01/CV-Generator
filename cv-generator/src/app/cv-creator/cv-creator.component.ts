54e95dda1afff15b696a89927e5df316e514cc7f
Jakub Budziński <budzinski212@o2.pl>
2024-12-30 18:25:21 +0100

    feat(cv creator): add get Summary items method

--- cv-generator/src/app/cv-creator/cv-creator.component.ts
+++ cv-generator/src/app/cv-creator/cv-creator.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CvStepperComponent } from './cv-stepper/cv-stepper.component';
+import { AboutMeStepService } from './cv-stepper/steps/about-me-step/about-me-step.service';
+import { ContactStepService } from './cv-stepper/steps/contact-step/contact-step.service';
+import { combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-cv-creator',
@@ -8,4 +11,26 @@ import { CvStepperComponent } from './cv-stepper/cv-stepper.component';
   templateUrl: './cv-creator.component.html',
   styleUrl: './cv-creator.component.scss',
 })
-export class CvCreatorComponent {}
+export class CvCreatorComponent {
+  constructor(
+    private readonly aboutMeService: AboutMeStepService,
+    private readonly contactService: ContactStepService
+  ) {
+    const summaryItems$ = this.getSummaryItems();
+
+    //console log only for test
+    summaryItems$.subscribe((x) => console.log(x));
+  }
+
+  private getSummaryItems() {
+    return combineLatest([
+      this.aboutMeService.summaryItems$,
+      this.contactService.summaryItems$,
+    ]).pipe(
+      map(([aboutMe, contact]) => ({
+        aboutMe,
+        contact,
+      }))
+    );
+  }
+}
